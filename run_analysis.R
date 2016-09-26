75d7e7823782e513a5822ddab030c09b243da70c
michsvob <svoboda.michal@seznam.cz>
2016-09-26 21:07:13 +0200

    Update run_analysis.R
    Fixed issue with missing Subject_ID

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 #Download and unzip files
 if(!file.exists("wearData.zip")){
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-              "wearData.zip",method = "curl")
+                "wearData.zip",method = "curl")
   unzip("wearData.zip")
 }
 
@@ -12,17 +12,21 @@ trainData<-read.table("UCI HAR Dataset/train/X_train.txt")
 testData<-read.table("UCI HAR Dataset/test/X_test.txt")
 testAct<-read.table("UCI HAR Dataset/test/y_test.txt")
 trainAct<-read.table("UCI HAR Dataset/train/y_train.txt")
+trainSubj<-read.table("UCI HAR Dataset/train/subject_train.txt")
+testSubj<-read.table("UCI HAR Dataset/test/subject_test.txt")
 
 #Name the columns
 names(actLabels)<-c("Activity_ID","Activity")
 names(testAct)<-c("Activity_ID")
 names(trainAct)<-c("Activity_ID")
+names(testSubj)<-c("Subject_ID")
+names(trainSubj)<-c("Subject_ID")
 names(trainData)<-make.names(featLabels$V2,unique = TRUE)
 names(testData)<-make.names(featLabels$V2,unique = TRUE)
 
 #Bind column with activity ID
-testData<-cbind(testData,testAct)
-trainData<-cbind(trainData,trainAct)
+testData<-cbind(testData,testAct,testSubj)
+trainData<-cbind(trainData,trainAct,trainSubj)
 
 #Bind column with translated activity ID (column Activity)
 testData<-merge(testData,actLabels)
@@ -34,13 +38,13 @@ mergedData<-rbind(testData,trainData)
 #Delete Activity_ID columns as it is no longer needed
 mergedData$Activity_ID<-NULL
 
-#Select just columns with mean and std
-colInd<-grep("mean|std|Activity",names(mergedData))
+#Select just columns with mean and std (and Activity ID and Subject ID)
+colInd<-grep("mean|std|Activity|Subject",names(mergedData))
 selection<-mergedData[,colInd]
 
 #Calculate averages for each variable for each activity
 library(dplyr)
-averages<-selection %>% group_by(Activity) %>% summarise_all(mean)
+averages<-selection %>% group_by(Activity,Subject_ID) %>% summarise_all(mean)
 averages
 
 write.table(averages,file = "dataset.txt",row.names = F)
